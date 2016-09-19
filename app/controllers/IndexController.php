3efab8353998423581a963097183cf4800f9e21c
Pepe <saraiptr@gmail.com>
2016-09-19 22:27:33 +0200

    Initial commit

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+
+class IndexController extends ControllerBase
+{
+
+	public function indexAction()
+	{
+
+	}
+
+	public function loginAction()
+	{
+
+	}
+
+	public function registerAction()
+	{
+
+	}
+
+	public function logoutAction()
+	{
+
+	}
+
+}
+
